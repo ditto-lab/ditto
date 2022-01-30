b2133cb329a3989146121500ac94a51029e28c4b
calvbore <calv.bore@gmail.com>
2022-01-30 14:31:50 -0600

    extra cleanup

--- src/DittoMachine.sol
+++ src/DittoMachine.sol
@@ -317,9 +317,7 @@ contract DittoMachine is ERC721, ERC721TokenReceiver {
 
         ERC721(ERC721Contract).safeTransferFrom(address(this), owner, id);
 
-        if (
-            IERC165(ERC721Contract).supportsInterface(_INTERFACE_ID_ERC2981) == true
-        ) {
+        if (IERC165(ERC721Contract).supportsInterface(_INTERFACE_ID_ERC2981) == true) {
             (address receiver, uint256 royaltyAmount) = IERC2981(ERC721Contract).royaltyInfo(
                 cloneShape.tokenId,
                 cloneShape.worth
@@ -328,7 +326,6 @@ contract DittoMachine is ERC721, ERC721TokenReceiver {
                 cloneShape.worth -= royaltyAmount;
                 SafeTransferLib.safeTransfer(
                     ERC20(ERC20Contract),
-                    // address(this),
                     receiver,
                     royaltyAmount
                 );
