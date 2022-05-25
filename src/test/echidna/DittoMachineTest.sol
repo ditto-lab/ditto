43251de8be756335204430a7786a0d8ee6a7c20f
0xbok <1689531+0xbok@users.noreply.github.com>
2022-05-26 00:28:18 +0530

    failing echidna

---
+++ src/test/echidna/DittoMachineTest.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8.4;
+//SPDX-License-Identifier: MIT
+
+import {DittoMachine} from "../../DittoMachine.sol";
+import {UnderlyingNFT} from "../UnderlyingNFT.sol";
+import {Currency} from "../TestBase.sol";
+
+contract DittoMachineTest {
+
+    DittoMachine dm;
+    UnderlyingNFT nft;
+    Currency cry;
+
+    constructor() {
+
+        dm = new DittoMachine();
+        nft = new UnderlyingNFT();
+        cry = new Currency();
+
+        cry.mint(address(this), type(uint).max);
+        cry.approve(address(dm), type(uint).max);
+    }
+
+    function duplicate(uint256 _tokenId, uint256 _amount, bool floor, uint256 index) external {
+        dm.duplicate(address(nft), _tokenId, address(cry), _amount, floor, index);
+    }
+
+    function dissolve(uint256 _tokenId, bool floor, uint256 index) external {
+        uint256 protoId = uint256(keccak256(abi.encodePacked(
+            address(nft),
+            _tokenId,
+            address(cry),
+            floor
+        )));
+        dm.dissolve(protoId, index);
+    }
+
+}
